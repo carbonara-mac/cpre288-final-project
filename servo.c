00b25c07edca4b66bc1c2019255764228f8babe7
Cao <dannycao@iastate.edu>
2021-11-18 17:10:49 -0600

    Change comments for better clarity, added TODO

--- servo.c
+++ servo.c
@@ -16,15 +16,16 @@ int currentAngle;
  * Ideal 0 degrees = 304000
  * Ideal 180 degrees = 288000
  *
- * Bot 13:
- *      - 0=312300
- *          - 0x4C3EC
- *      - 180=284000
- *          - 0x45560
- *      - 90=297600
- *          - 0x48A80
- *
  */
+
+/********CALIBRATION INFORMATION***************
+ *
+ *  Bot 13:
+ *      0 = 312300 = 0x4C3EC
+ *     90 = 297600 = 0x48A80
+ *    180 = 284000 = 0x45560
+ */
+
 void servo_init1(void)
 {
     done = 0;
@@ -34,51 +35,44 @@ void servo_init1(void)
     GPIO_PORTB_AFSEL_R |= 0b100000; // Sets PB5 to alternate function
     GPIO_PORTB_PCTL_R |= 0x00700000; // Selects timer 1 (T1CCP1)
     GPIO_PORTB_PCTL_R &= 0xFF7FFFFF;
-
     GPIO_PORTB_DEN_R |= 0b00100000; // enable PB5 digital function
     GPIO_PORTB_DIR_R |= 0b00100000; // Set PB5 to output
 
     TIMER1_CTL_R &= 0b011111111; // disable timer1B to config
-
-    //Initialize Timer Registers
-    //TIMER1_CTL_R |= 0b110000000000; //Enable both edge trigger for T(b)EVENT
     TIMER1_CFG_R |= 0x4; //Use split 16-bit timers
-
     TIMER1_TBMR_R |= 0b01010; //Set as count down, periodic, PWM mode
     TIMER1_TBMR_R &= 0b01010;
-
     TIMER1_TBPR_R = 0x04; // Loads MSBs of 0x4E200 (320,000)
     TIMER1_TBILR_R = 0xE200; // Loads 8 LSBs of 0x4E200 (Period = 320,000 clock cycles)
 
-//        TIMER1_TBMATCHR_R = 0xA380; // Sets match to 304,000 (1ms = 0 degrees)
-//        TIMER1_TBPMR_R = 0x04; // Prescalar for 304,000
+//    Theoretical Match Value ->    TIMER1_TBMATCHR_R = 0xA380; // Sets match to 304,000 (1ms = 0 degrees)
+//                                  TIMER1_TBPMR_R = 0x04; // Prescalar for 304,000
 
     TIMER1_CTL_R |= 0b100000000; // Enable timer b
 
-    servo_move1(90);
-//    TIMER1_TBMATCHR_R = 0x8A80; // 90 degrees BOT 13
-//    TIMER1_TBPMR_R = 0x04;
-
+    servo_move1(90); //Input 90 degrees for initialization
 }
 
 int servo_move1(int degrees)
 {
-    if (degrees < 0)
+    if (degrees < 0) //For situations where input degrees may be less than 0
     {
         degrees = 0;
     }
-    if (degrees > 180)
+    if (degrees > 180) //For situations where input degrees may be more than 0
     {
         degrees = 180;
     }
-    int match = convertDegreesBOT13(degrees);
+
+    int match = convertDegreesBOT13(degrees); //Equation function to convert the degree into the match value
+
     TIMER1_CTL_R &= 0b011111111;
-    TIMER1_TBMATCHR_R = match; // 0 degrees BOT 13
-    TIMER1_TBPMR_R = match >> 16;
+    TIMER1_TBMATCHR_R = match; // Match value for specified degree
+    TIMER1_TBPMR_R = match >> 16; //Prescale Value for specified degree
     TIMER1_CTL_R |= 0b100000000; // Enable timer b
 
-    timer_waitMillis((abs(degrees-currentAngle) / 200) / 1000);
-    timer_waitMillis(50);
+    timer_waitMillis((abs(degrees - currentAngle) / 200) / 1000);
+    timer_waitMillis(50); //Delay for the ping and IR sensors to have enough time to gather information
 
     currentAngle = degrees;
     return match;
@@ -87,7 +81,8 @@ int servo_move1(int degrees)
 
 int convertDegreesBOT13(int degrees)
 {
-    int match = 0.0679012 * pow((degrees - 1247.73), 2) + 206590;
+    //TODO: Implement calibration method to have match equation consistent through out different bots
+    int match = 0.0679012 * pow((degrees - 1247.73), 2) + 206590; //IMPORTANT: Equation for Bot13
     return match;
 }
 
