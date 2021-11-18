78e5d3057ad6874257656b735b8abf1a42a4fcc5
jamesminardi <jamesminardip@gmail.com>
2021-11-18 16:33:27 -0600

    Added callibration skeleton #define if statements

--- main.c
+++ main.c
@@ -4,10 +4,41 @@
  * main.c
  */
 
+#define _SERVO_CALLIBRATION 1
+#define _IR_CALLIBRATION 0
+#define _MAIN 0
+
+
 #include <stdio.h>
+
 
 int main(void)
 {
-    printf("Hello, world!"); //danny was here
-    return 0;
+
+#if _SERVO_CALLIBRATION
+
+
+
+
+
+
+
+
+
+
+
+#endif // _SERVO_CALLIBRATION
+
+#if _IR_CALLIBRATION
+
+#endif // _IR_CALLIBRATRION
+
+#if _MAIN
+
+#endif // _MAIN
+
+
+
+
+
 }
