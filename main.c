5e1cd6f57ca5c45a36a9202d6596f584de513bef
jamesminardi <jamesminardip@gmail.com>
2021-11-28 01:58:41 -0600

    redundant commit

--- main.c
+++ main.c
@@ -1,5 +1,3 @@
-
-
 /**
  * main.c
  */
@@ -8,25 +6,20 @@
 #define _IR_CALLIBRATION 0
 #define _MAIN 0
 
-
 #include <stdio.h>
+#include "scan.h"
 
+/*
+ * Run _SERVO_CALIBRATION by itself to find values for these. Currently set for bot06
+ */
+int right_calibration_value = 313984; // Run _SERVO_CALLIBRATION by itself to find values
+int left_calibration_value = 286848;
 
 int main(void)
 {
 
 #if _SERVO_CALLIBRATION
-
-//iwuhauifhui
-
-
-
-
-
-
-
-
-
+    servo_calibration();
 #endif // _SERVO_CALLIBRATION
 
 #if _IR_CALLIBRATION
@@ -36,9 +29,5 @@ int main(void)
 #if _MAIN
 
 #endif // _MAIN
-
-
-
-
 
 }
