8183823e6a52d50ecd4c3aaf2ffcd235ea9dfe4f
James Minardi <77563712+jamesminardi@users.noreply.github.com>
2021-12-04 01:45:15 -0600

    Added method stubs

--- user_input.h
+++ user_input.h
@@ -1,8 +1,10 @@
 /*
- * user_input.h
+ *  user_inptut.h
  *
- *  Created on: Dec 3, 2021
- *      Author: jminardi
+ *  Parses and executes commands received from user input
+ *  
+ *  Created on: Nov 30, 2021
+ *  Author: Ainara Machargo, James Minardi
  */
 
 #ifndef USER_INPUT_H_
@@ -10,6 +12,26 @@
 
 #include <stdio.h>
 
+/* 
+ * Parses given char pointer by the following formats:
+ * 		"[Function],[Parameter]\0"
+ *		"[Function]\0"
+ * [Function] is a single char representing a function to complete: W,A,S,D,M,Z
+ * [Parameter] is an integer value to give to the desired function: Distance or Angle
+ * 
+ *  @param command - Null terminated char pointer to parse
+ */
+void ui_parse(char *command);
 
+/*
+ * Execute desired function with given integer parameter.
+ * 		w: Move forward, s: move backward, a: rotate counter clockwise, d: rotate clockwise
+ *		m: scan 0-180 degrees or specified degrees
+ *		z: finish program
+ *	
+ * @param function - Single char to represent desired instructions to execute
+ * 		  parameter - Integer value to give the function call as a distance or angle
+ */
+void ui_execute(char function, int parameter);
 
 #endif /* USER_INPUT_H_ */
