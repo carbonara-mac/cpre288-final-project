2b882b2371e53f9666367274d0d27078293334ae
James Minardi <77563712+jamesminardi@users.noreply.github.com>
2021-12-04 01:45:20 -0600

    Updated skeleton to ui_execute

--- user_input.c
+++ user_input.c
@@ -1,19 +1,31 @@
 /*
  *  user_inptut.c
  *
+ *  Parses and executes commands received from user input
+ *  
  *  Created on: Nov 30, 2021
  *  Author: Ainara Machargo, James Minardi
  */
 
 #include "user_input.h"
 
+
+/* 
+ * Parses given char pointer by the following formats:
+ * 		"[Function],[Parameter]\0"
+ *		"[Function]\0"
+ * [Function] is a single char representing a function to complete: W,A,S,D,M,Z
+ * [Parameter] is an integer value to give to the desired function: Distance or Angle
+ * 
+ *  @param command - Null terminated char pointer to parse
+ */
 void ui_parse(char *command)
 {
     char function = *command;
     int parameter;
     command++;
 
-    if (&command == '\0')
+    if (*command == '\0')
     {
         // One character command (ex. Scan0-180 (m) and quit (z))
         parameter = -1;
@@ -29,90 +41,44 @@ void ui_parse(char *command)
         }
     }
 
-}
+} // END ui-parse
 
+
+/*
+ * Execute desired function with given integer parameter.
+ * 		w: Move forward, s: move backward, a: rotate counter clockwise, d: rotate clockwise
+ *		m: scan 0-180 degrees or specified degrees
+ *		z: finish program
+ *	
+ * @param function - Single char to represent desired instructions to execute
+ * 		  parameter - Integer value to give the function call as a distance or angle
+ */
 void ui_execute(char function, int parameter) {
-
-    // Case statements for function argument
-    //      -> Call different functions based on function arg and the parameter int
-    //      -> Ex. function=w and paramter=25, then call moveForward for 25 centimeters
-
-}
-
-
-main(void) {
-
-volatile  char uart_data;  // Your UART interupt code can place read data here
-volatile  char flag;       // Your UART interupt can update this flag
-                           // to indicate that it has placed new data
-                           // in uart_data
-						    // ISSUE WITH oi_init() function. Program stops there...THIS ONLY HAPPENS WHEN ROOMBA IS OFF (PRESS "CLEAN" BUTTON)
-oi_t *sensor_data = oi_alloc();
-oi_init(sensor_data);
-
-//cyBot_uart_init(); // initialize connection via wifi from Putty to CyBot
-uart_init(115200);
-uart_interrupt_init();
-
-lcd_init(); // initialize lcd screen
-
-ping_init();
-adc_init();
-servo_init();
-
-char keyPressMsg[] = "Got an  \n\r";
-int valIndex = 7; //num for message
-char fbMsg[] = "How many milis?";
-char lrMsg[] = "How many degrees?";
-
-int inputNum = 0;
-while (1){
-
-  if (flag) {
-            flag = 0;
-
-            lcd_clear();
-            lcd_putc(uart_data); // displays byte from putty
-            keyPressMsg[valIndex] = uart_data; // updates putty string with current keypress
-            uart_sendStr(keyPressMsg);
-        }
-
-	//forward
-	if(uart_data == 'w' ){
-
-	inputNum = uart_data;
-
-	oi_setWheels(inputNum, inputNum);
+	switch(function) {
+		case w:
+			// Move forward parameter centimeters
+			break;
+		case s:
+			// Move backward parameter centimeters
+			break;
+		case a:
+			// Rotate counter clockwise parameter degrees
+			break;
+		case d:
+			// Rotate clockwise parameter degrees
+			break;
+		case m:
+			if (parameter == -1) {
+				// Scan 0 to 180	
+			} else {
+				// Scan at parameter degrees
+			}
+			break;
+		case z:
+			// Program completion
+			break;
+		default:
+			// Shouldn't ever go here
+			break;
 	}
-	//backward
-	if(uart_data == 's' ){
-	uart_sendStr(fbMsg);
-	inputNum = uart_data;
-	oi_setWheels( -1 * inputNum, -1 * inputNum);
-	inputNum=0;
-	}
-	//rotate right
-	if(uart_data == 'd' ){
-	uart_sendStr(lrMsg);
-	inputNum = uart_data;
-	oi_setWheels( -1 * inputNum, inputNum);
-	inputNum=0;
-	}
-	//rotate left
-	if(uart_data == 'a' ){
-	uart_sendStr(lrMsg);
-	inputNum = uart_data;
-
-	oi_setWheels(inputNum, -1* inputNum);
-	inputNum=0;
-	}
-
-	//TODO
-	//scan 0-180
-	//scan in general
-	//print diagnostics
-	//music command
-	//end command
-
-}
-}
+} // END ui_execute
